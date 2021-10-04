f4b25c91021514269585f1d9223402ef2f9c17dc
Jason <fr33.L4nc3r@live.co.uk>
2021-10-04 18:36:57 +0100

    Dos

---
+++ Double D/Double D/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Double_D
+{
+    class Controller
+    {
+        private Vehicle vehicle;
+        private string personality;
+        int team;
+        public Controller(Vehicle vehicle, string personality, int team)
+        {
+            this.Vehicle = vehicle;
+            this.Personality = personality;
+            this.Team = team;
+        }
+
+        public string Personality { get => personality; set => personality = value; }
+        public int Team { get => team; set => team = value; }
+        internal Vehicle Vehicle { get => vehicle; set => vehicle = value; }
+    }
+}
