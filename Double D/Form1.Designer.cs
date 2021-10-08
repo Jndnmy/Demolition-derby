93f529466d4640163a92d811f570d812bf4afbae
Kacper Krzeminski <kacperkrzeminski123@gmail.com>
2021-10-08 20:27:30 +0200

    Revert "a"
    This reverts commit 69722661f301b50d7e5b77f42a011f344dc5c2fa.

--- Double D/Form1.Designer.cs
+++ Double D/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Double_D
+﻿namespace Double_D
 {
     partial class Form1
     {
@@ -29,20 +28,10 @@ namespace Double_D
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load_1);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            this.ResumeLayout(false);
-
         }
 
         #endregion
